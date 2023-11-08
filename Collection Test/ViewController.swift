d41cb6f2e59648886d3181183736abc9742eef20
dimaboga <bogatireff.d@yandex.kz>
2023-11-08 16:03:07 +0600

    Initial Commit

---
+++ Collection Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection Test
+//
+//  Created by Дмитрий Богатырев on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
